79ed8441b396bcdb9f51a6edba53d89d9328d73d
lrtraviteja <lrtraviteja@gmail.com>
2025-12-01 11:09:32 +0530

    Debug 3

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,7 +4,9 @@ const nodemailer = require('nodemailer');
 function createTransporter() {
   if (process.env.EMAIL_USER && process.env.EMAIL_PASS) {
     return nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
       auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS }
     });
   }
